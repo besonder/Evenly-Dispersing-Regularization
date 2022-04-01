d4a22e947da5a65eb24b26fe13eb68ee7660e518
bsd <bad@icode-lab.com>
2022-04-01 09:09:01 +0000

    v1.0

--- train.py
+++ train.py
@@ -15,6 +15,7 @@ import cad
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--model', type=str, default='resnet18')
+parser.add_argument('--data', type=str, default='cifar100')
 parser.add_argument('--gpu', type=str, default='0')
 parser.add_argument('--reg', type=str, default=None)
 parser.add_argument('--r', type=float, default=0.1)
@@ -56,27 +57,86 @@ if args.distributed:
     torch.distributed.init_process_group(backend='nccl', init_method='env://')
 
 
-train_dataset = torchvision.datasets.CIFAR100(
-                root='./DATA/', 
-                transform=transforms.Compose(
-                    [
-                    transforms.RandomResizedCrop(224),
-                    transforms.RandomHorizontalFlip(),
-                    transforms.ToTensor(),
-                    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
-                    ]),
-                train=True)
+if args.model == 'resnet18':
+    model = torchvision.models.resnet18()
+elif args.model == 'resnet34':
+    model = torchvision.models.resnet34()
+elif args.model == 'resnet50':
+    model = torchvision.models.resnet50()
+elif args.model == 'resnet101':
+    model = torchvision.models.resnet101()
+elif args.model == 'resnet152':
+    model = torchvision.models.resnet152()
 
-val_dataset = torchvision.datasets.CIFAR100(
-                root='./DATA/', 
-                transform=transforms.Compose(
-                    [
-                    transforms.Resize(256),
-                    transforms.CenterCrop(224),
-                    transforms.ToTensor(),
-                    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
-                    ]),
-                train=False)
+
+if args.data == 'cifar100':
+    model.fc = torch.nn.Linear(in_features=512, out_features=100, bias=True)
+
+    train_dataset = torchvision.datasets.CIFAR100(
+                    root='./DATA/', 
+                    transform=transforms.Compose(
+                        [
+                        transforms.RandomHorizontalFlip(),
+                        transforms.ToTensor(),
+                        transforms.Normalize((0.5071, 0.4865, 0.4409), (0.2673, 0.2564, 0.2762))
+                        ]),
+                    train=True)
+
+    val_dataset = torchvision.datasets.CIFAR100(
+                    root='./DATA/', 
+                    transform=transforms.Compose(
+                        [
+                        transforms.ToTensor(),
+                        transforms.Normalize((0.5071, 0.4865, 0.4409), (0.2673, 0.2564, 0.2762))
+                        ]),
+                    train=False)
+
+elif args.data == 'cifar10':
+    model.fc = torch.nn.Linear(in_features=512, out_features=10, bias=True)
+
+    train_dataset = torchvision.datasets.CIFAR10(
+                    root='./DATA/', 
+                    transform=transforms.Compose(
+                        [
+                        transforms.RandomHorizontalFlip(),
+                        transforms.ToTensor(),
+                        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2470, 0.2435, 0.2616))
+                        ]),
+                    train=True)
+
+    val_dataset = torchvision.datasets.CIFAR10(
+                    root='./DATA/', 
+                    transform=transforms.Compose(
+                        [
+                        transforms.ToTensor(),
+                        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2470, 0.2435, 0.2616))
+                        ]),
+                    train=False)  
+
+elif args.data == 'imagenet':
+    train_dataset = torchvision.datasets.ImageFolder(
+                    root='./DATA/', 
+                    transform=transforms.Compose(
+                        [
+                        transforms.RandomResizedCrop(224),
+                        transforms.RandomHorizontalFlip(),
+                        transforms.ToTensor(),
+                        transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
+                        ]),
+                    train=True)
+
+    val_dataset = torchvision.datasets.ImageFolder(
+                    root='./DATA/', 
+                    transform=transforms.Compose(
+                        [
+                        transforms.Resize(256),
+                        transforms.CenterCrop(224),
+                        transforms.ToTensor(),
+                        transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
+                        ]),
+                    train=False)    
+
+model.cuda() 
 
 if args.distributed:
     trainsampler = DistributedSampler(train_dataset)
@@ -103,22 +163,6 @@ val_loader = DataLoader(
                     pin_memory=True, 
                     sampler=validsampler
                     )
-
-if args.model == 'resnet18':
-    model = torchvision.models.resnet18()
-elif args.model == 'resnet34':
-    model = torchvision.models.resnet34()
-elif args.model == 'resnet50':
-    model = torchvision.models.resnet50()
-elif args.model == 'resnet101':
-    model = torchvision.models.resnet101()
-elif args.model == 'resnet152':
-    model = torchvision.models.resnet152()
-
-
-model.fc = torch.nn.Linear(in_features=512, out_features=100, bias=True)
-model.cuda()
-
 
 criterion = nn.CrossEntropyLoss().cuda()
 
