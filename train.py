55e6f12f07002277cdd9fb136969f21590ef50a8
ncianeo <neo@icode-lab.com>
2022-04-21 16:53:50 +0900

    yaml config 쓰도록 코드 수정

--- train.py
+++ train.py
@@ -7,32 +7,17 @@ from torch.nn.parallel import DistributedDataParallel as DDP
 from torch.utils.data import DataLoader
 import torch
 import torch.nn as nn
-import torchvision.transforms as transforms
 from torch.utils.data.distributed import DistributedSampler
 from utils import reg_losses, model_datasets
 from utils.utils import AverageMeter, adjust_learning_rate, accuracy, reg_weights
-from utils.milestone import milestones
+from utils.config import load_config
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--model', type=str, default='resnet18')
-parser.add_argument('--data', type=str, default='cifar100')
+parser.add_argument('--experiment_config', '-e', type=str,
+                    default='configs/resnet18_cifar100_SO.yml')
 parser.add_argument('--gpu', type=str, default='0')
-
-parser.add_argument('--reg', type=str, default='ADC')
-# parser.add_argument('--r', type=float, default=0)
-parser.add_argument('--rtn', type=float, default=1e-1)
-parser.add_argument('--fc', type=bool, default=False)
-parser.add_argument('--onlyconv', type=bool, default=True)
-
-parser.add_argument('--theta', type=float, default=1.5708)
-
-# parser.add_argument('--lr', type=float, default=0.1)
-parser.add_argument('--epochs', type=int, default=200)
-parser.add_argument('--bsize', type=int, default=128)
 parser.add_argument('--num_worker', type=int, default=4)
-parser.add_argument('--warm', type=int, default=1)
-
 parser.add_argument('--local_rank', type=int, default=0)
 
 args = parser.parse_args()
@@ -43,7 +28,7 @@ for i, arg in enumerate(sys.argv[1:]):
     # elif "--" in arg:
     #     args.__setattr__(arg[2:], str(sys.argv[1:][i+1]))
 
-milestones(args)
+load_config(args)
 
 os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu
 os.environ['OMP_NUM_THREADS'] = str(4)
@@ -66,10 +51,11 @@ if args.distributed:
 
 model, train_dataset, val_dataset = model_datasets.model_data(args)
 
-model.cuda() 
+model.cuda()
 
 if args.distributed:
-    model = DDP(model, device_ids=[args.local_rank], output_device=args.local_rank)
+    model = DDP(model, device_ids=[args.local_rank],
+                output_device=args.local_rank)
     trainsampler = DistributedSampler(train_dataset)
     validsampler = DistributedSampler(val_dataset)
 else:
@@ -79,38 +65,38 @@ else:
 if not args.distributed or args.local_rank == 0:
     now = datetime.now()
     logfile = '_'.join([args.model, args.data, args.reg])
-    logfile += now.strftime("_%Y-%m-%d_%H-%M-%S") 
+    logfile += now.strftime("_%Y-%m-%d_%H-%M-%S")
     logfile += '.txt'
 
     f = open(os.path.join('./log/', logfile), 'w')
 
     for k in args.__dict__:
         print(k + ': ' + str(getattr(args, k)))
-        f.write(k + ': ' + str(getattr(args, k)) +'\n')
+        f.write(k + ': ' + str(getattr(args, k)) + '\n')
 
 train_loader = DataLoader(
-                    train_dataset, 
-                    batch_size=args.bsize, 
-                    shuffle=trainsampler is None, 
-                    num_workers=args.num_worker, 
-                    pin_memory=True, 
-                    sampler=trainsampler
-                    )
+    train_dataset,
+    batch_size=args.bsize,
+    shuffle=trainsampler is None,
+    num_workers=args.num_worker,
+    pin_memory=True,
+    sampler=trainsampler
+)
 
 val_loader = DataLoader(
-                    val_dataset, 
-                    batch_size=args.bsize, 
-                    shuffle=False, 
-                    num_workers=args.num_worker, 
-                    pin_memory=True, 
-                    sampler=validsampler
-                    )
+    val_dataset,
+    batch_size=args.bsize,
+    shuffle=False,
+    num_workers=args.num_worker,
+    pin_memory=True,
+    sampler=validsampler
+)
 
 criterion = nn.CrossEntropyLoss().cuda()
 
 regularizer = args.reg
 
-optimizer = torch.optim.SGD(model.parameters(), 
+optimizer = torch.optim.SGD(model.parameters(),
                             lr=args.lr[0],
                             momentum=0.9,
                             weight_decay=args.wr[0]
@@ -124,7 +110,7 @@ time_t = AverageMeter('Time', ':6.2f')
 for epoch in range(args.epochs):
 
     adjust_learning_rate(optimizer, epoch, args)
-    
+
     losses_t = AverageMeter('Loss', ':.4e')
     top1_t = AverageMeter('Acc@1', ':6.2f')
     top5_t = AverageMeter('Acc@5', ':6.2f')
@@ -136,47 +122,46 @@ for epoch in range(args.epochs):
         target = target.cuda(non_blocking=True)
         output = model(images)
 
-        loss = criterion(output, target) 
+        loss = criterion(output, target)
 
         if args.reg != 'base':
-            loss += reg_losses.reg_loss(args, kern_weights, conv_weights, model)
+            loss += reg_losses.reg_loss(args,
+                                        kern_weights, conv_weights, model)
 
         acc1, acc5 = accuracy(output, target, topk=(1, 5))
-        
+
         losses_t.update(loss.item(), images.size(0))
         top1_t.update(acc1[0], images.size(0))
         top5_t.update(acc5[0], images.size(0))
-        
+
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
 
     time_t.update(time()-stime)
-    
+
     ####### validataion #######
-    
+
     losses = AverageMeter('Loss', ':.4e')
     top1 = AverageMeter('Acc@1', ':6.2f')
     top5 = AverageMeter('Acc@5', ':6.2f')
-    
+
     model.eval()
     for i, (images, target) in enumerate(val_loader):
         images = images.cuda(non_blocking=True)
         target = target.cuda(non_blocking=True)
         output = model(images)
 
-        loss = criterion(output, target) 
-        acc1, acc5 = accuracy(output, target, topk=(1, 5))    
-        
+        loss = criterion(output, target)
+        acc1, acc5 = accuracy(output, target, topk=(1, 5))
+
         losses.update(loss.item(), images.size(0))
         top1.update(acc1[0], images.size(0))
         top5.update(acc5[0], images.size(0))
     if not args.distributed or args.local_rank == 0:
         print(f'epoch: {epoch}, tr_time: {time_t.val:.1f}, validation loss: {losses.avg:.3f}, acc1: {top1.avg:.3f}, acc5: {top5.avg:.3f}')
-        f.write(f'epoch: {epoch}, tr_time: {time_t.val:.1f}, validation loss: {losses.avg:.3f}, acc1: {top1.avg:.3f}, acc5: {top5.avg:.3f}\n')
-    
+        f.write(
+            f'epoch: {epoch}, tr_time: {time_t.val:.1f}, validation loss: {losses.avg:.3f}, acc1: {top1.avg:.3f}, acc5: {top5.avg:.3f}\n')
+
 if not args.distributed or args.local_rank == 0:
     f.close()
-
-
-
